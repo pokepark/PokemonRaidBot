db78ec0cb9026ff9423b84f8f78ad6ccf3782a08
klablabla <30940278+klablabla@users.noreply.github.com>
2018-06-03 04:31:45 +0200

    Changed "cleanup" to "cleanup_raids"

--- sql/pokemon-raid-bot.sql
+++ sql/pokemon-raid-bot.sql
@@ -30,7 +30,7 @@ CREATE TABLE `attendance` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `cleanup` (
+CREATE TABLE `cleanup_raids` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `raid_id` int(10) unsigned NOT NULL,
   `chat_id` bigint(20) NOT NULL,
