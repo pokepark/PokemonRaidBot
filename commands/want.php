075e1d83cdcc5197ddee682253fb4a8e3e0f601b
klablabla <30940278+klablabla@users.noreply.github.com>
2018-06-24 10:15:04 +0200

    added command /want

---
+++ commands/want.php
@@ -0,0 +1,34 @@
+<?php
+// Write to log.
+debug_log('want()');
+
+// For debug.
+//debug_log($update);
+//debug_log($data);
+
+// Get want code.
+$want_code = trim(substr($update['message']['text'], 6));
+
+// Write to log.
+debug_log('SET want code to ' . $want_code);
+
+// Private chat type.
+if ($update['message']['chat']['type'] == 'private' || $update['callback_query']['message']['chat']['type'] == 'private') {
+    // Update want code in users table.
+    my_query(
+        "
+        UPDATE    users
+        SET       want = '{$db->real_escape_string($want_code)}'
+          WHERE   user_id = {$update['message']['from']['id']}
+        "
+    );
+
+    // Send the message.
+    sendMessage($update['message']['chat']['id'], getTranslation('want_update'));
+
+} else {
+    sendMessage($update['message']['chat']['id'], 'Mislukt');
+    
+}
+
+exit();
