8805aa55c128b23c909b25ef25d3c45d4cdf3a8c
klablabla <30940278+klablabla@users.noreply.github.com>
2018-06-24 20:43:37 +0200

    /friend will filter the 12 numbers

--- commands/friend.php
+++ commands/friend.php
@@ -7,28 +7,44 @@ debug_log('friend()');
 //debug_log($data);
 
 // Get friend code.
-$friend_code = trim(substr($update['message']['text'], 7));
+//$friend_code = trim(substr($update['message']['text'], 7));
 
-// Write to log.
-debug_log('SET friend code to ' . $friend_code);
+//Check is 12 digit number
+$friend_code = preg_replace('/[^[:digit:]]/', '',(substr($update['message']['text'], 7)));
 
-// Private chat type.
-if ($update['message']['chat']['type'] == 'private' || $update['callback_query']['message']['chat']['type'] == 'private') {
-    // Update friend code in users table.
-    my_query(
-        "
-        UPDATE    users
-        SET       friend_code = '{$db->real_escape_string($friend_code)}'
-          WHERE   user_id = {$update['message']['from']['id']}
-        "
-    );
+if (strlen($friend_code) == 12) {
+		
+	// Write to log.
+	debug_log('SET friend code to ' . $friend_code);
 
-    // Send the message.
-    sendMessage($update['message']['chat']['id'], getTranslation('friend_code_update'));
+	// Private chat type.
+	if ($update['message']['chat']['type'] == 'private' || $update['callback_query']['message']['chat']['type'] == 'private') {
+		// Update friend code in users table.
+		my_query(
+			"
+			UPDATE    users
+			SET       friend_code = '{$db->real_escape_string($friend_code)}'
+			  WHERE   user_id = {$update['message']['from']['id']}
+			"
+		);
 
+		// Send the message.
+		sendMessage($update['message']['chat']['id'], getTranslation('friend_code_update'));
+
+	} else {
+		sendMessage($update['message']['chat']['id'], getTranslation('friend_code_update_fail'));
+		
+	}
+	
 } else {
-    sendMessage($update['message']['chat']['id'], getTranslation('fail'));
-    
+	
+	// Write to log.
+	debug_log('Invalid friend code: ' . $friend_code);
+	
+	//Invalid code
+	sendMessage($update['message']['chat']['id'], getTranslation('friend_code_invalid'));
+	
 }
+
 
 exit();
