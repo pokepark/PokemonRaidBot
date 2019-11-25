d48f81ed6087f20a6af05bc24ddce034b83b7fc7
klablabla <30940278+klablabla@users.noreply.github.com>
2019-11-25 10:50:45 +0100

    Update vote_refresh.php

--- mods/vote_refresh.php
+++ mods/vote_refresh.php
@@ -2,7 +2,9 @@
 // Send the vote response.
 if($data['arg'] == "new") {
     send_response_vote($update, $data,false,false,true);
-}else {
-    send_response_vote($update, $data,false,false);
+}else if (RAID_PICTURE == true){
+   send_response_vote($update, $data,false,false); 
+} else {
+   send_response_vote($update, $data); 
 }
 exit();
