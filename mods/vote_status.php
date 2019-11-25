6e614389cd73bb99db9d595cc5c5f81efc028d32
klablabla <30940278+klablabla@users.noreply.github.com>
2019-11-25 10:47:14 +0100

    Update vote_status.php

--- mods/vote_status.php
+++ mods/vote_status.php
@@ -42,7 +42,11 @@ if (!empty($answer)) {
     );
 
     // Send vote response.
-    send_response_vote($update, $data,false,false);
+   if(RAID_PICTURE == true) {
+	    send_response_vote($update, $data,false,false);
+    } else {
+	    send_response_vote($update, $data);
+    } 
 } else {
     // Send vote time first.
     send_vote_time_first($update);
