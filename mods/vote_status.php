4cec704b0f8419c42d9f1ca84910f381fbdab97f
klablabla <30940278+klablabla@users.noreply.github.com>
2019-11-25 10:19:22 +0100

    Fixed status vote

--- mods/vote_status.php
+++ mods/vote_status.php
@@ -42,7 +42,7 @@ if (!empty($answer)) {
     );
 
     // Send vote response.
-    send_response_vote($update, $data);
+    send_response_vote($update, $data,false,false);
 } else {
     // Send vote time first.
     send_vote_time_first($update);
