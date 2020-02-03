bfd19e8d0b268a4cdddc3d058474e94675bda950
Florian Becker <fb@vxapps.com>
2020-02-03 14:24:12 +0100

    Fix gym names with single quotes in name to import

--- mods/importal.php
+++ mods/importal.php
@@ -18,7 +18,7 @@ if(defined('PORTAL_IMPORT') && PORTAL_IMPORT == true) {
     // Insert gym.
     try {
 
-        global $db;
+        global $dbh;
 
         // Gym name.
         $gym_name = $portal;
@@ -38,6 +38,7 @@ if(defined('PORTAL_IMPORT') && PORTAL_IMPORT == true) {
         }
 
         // Build query to check if gym is already in database or not
+        // TODO: Use PDO here
         $rs = my_query("
         SELECT    id, COUNT(*)
         FROM      gyms
@@ -74,7 +75,7 @@ if(defined('PORTAL_IMPORT') && PORTAL_IMPORT == true) {
 
         // Insert / Update.
         $statement = $dbh->prepare($query);
-        $statement->bindValue(':gym_name', $gym_name, PDO::PARAM_STR);
+        $statement->bindValue(':gym_name', dbh->quote($gym_name), PDO::PARAM_STR);
         $statement->bindValue(':lat', $lat, PDO::PARAM_STR);
         $statement->bindValue(':lon', $lon, PDO::PARAM_STR);
         $statement->bindValue(':address', $address, PDO::PARAM_STR);
