f4b78e0fc1e63dfe55f22e65d3a1d68d0e269ef0
Artanicus <artanicus@nocturnal.fi>
2020-02-23 09:37:47 +0200

    Make overview debug easier to grep for

--- mods/overview_share.php
+++ mods/overview_share.php
@@ -48,8 +48,7 @@ while ($rowRaids = $request_active_raids->fetch_assoc()) {
 }
 
 // Write to log.
-debug_log('Active raids:');
-debug_log($raids_active);
+debug_log($raids_active, 'Active raids for overview:');
 
 // Init empty active chats array.
 $chats_active = [];
@@ -60,8 +59,7 @@ if ($count_active_raids > 0) {
     $raid_ids_active = implode(',',$raid_ids_active);
 
     // Write to log.
-    debug_log('IDs of active raids:');
-    debug_log($raid_ids_active);
+    debug_log($raid_ids_active, 'IDs of active raids for overview:');
 
     // Get chat for active raids.
     if ($chat_id == 0) {
@@ -92,8 +90,7 @@ if ($count_active_raids > 0) {
 }
 
 // Write to log.
-debug_log('Active chats:');
-debug_log($chats_active);
+debug_log($chats_active, 'Active chats for overview:');
 
 // Get raid overviews
 if ($chat_id == 0) {
