ff0c3681f4b36416b93576b242fd1ebaa215484c
Artanicus <artanicus@nocturnal.fi>
2020-08-22 11:49:42 +0300

    Include asset_suffix in schema upgrades

--- sql/upgrade/2.0.235.8.sql
+++ sql/upgrade/2.0.235.8.sql
@@ -1,5 +1,6 @@
 ALTER TABLE `pokemon` ADD `pokemon_form_name` varchar(45) DEFAULT NULL;
 ALTER TABLE `pokemon` ADD `pokemon_form_id` int(4) DEFAULT NULL;
+ALTER TABLE `pokemon` ADD `asset_suffix` varchar(45) DEFAULT NULL;
 ALTER TABLE `pokemon` DROP `pokemon_form`;
 
 ALTER TABLE `raids` MODIFY `pokemon` int(4) DEFAULT NULL;
