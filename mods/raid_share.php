0f856907601077487fdc1196c8a2116ead69af94
hericle <mathias@studierenplus.de>
2020-10-11 17:34:09 +0200

    fix forgotten changes in other PR

--- mods/raid_share.php
+++ mods/raid_share.php
@@ -48,7 +48,7 @@ $raid_picture_hide_pokemon = explode(",",$config->RAID_PICTURE_HIDE_POKEMON);
 
 $raid_pokemon_id = $raid['pokemon'];
 $raid_level = get_raid_level($raid['pokemon'], $raid['pokemon_form']);
-$raid_pokemon_form_name = get_pokemon_form_name($raid_pokemon_id,$raid['pokemon_form_id']);
+$raid_pokemon_form_name = get_pokemon_form_name($raid_pokemon_id,$raid['pokemon_form']);
 $raid_pokemon = $raid_pokemon_id . "-" . $raid_pokemon_form_name;
 
 if($config->RAID_PICTURE && !in_array($raid_level, $raid_picture_hide_level) && !in_array($raid_pokemon, $raid_picture_hide_pokemon) && !in_array($raid_pokemon_id, $raid_picture_hide_pokemon)) {
