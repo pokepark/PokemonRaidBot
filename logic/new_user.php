b662224bed3230cabab5a6b2c0028c3042f35aca
Ossi Kuisma <52629375+Ninjasoturi@users.noreply.github.com>
2020-10-23 11:20:25 +0300

    Altered new_user and added user_tutorial

--- logic/new_user.php
+++ logic/new_user.php
@@ -1,26 +1,11 @@
 <?php
 /**
- * Check if the user has completed the tutorial
+ * Check if the user is new (has not completed tutorial)
  * @param $user_id
  * @return bool
  */
 function new_user($user_id) {
-    global  $dbh;
-	debug_log("Checking for new user: ".$user_id);
-    try {
-        $query = "SELECT tutorial FROM users WHERE user_id = :user_id LIMIT 1";
-        $statement = $dbh->prepare( $query );
-        $statement->execute([":user_id"=>$user_id]);
-        $res = $statement->fetch();
-        debug_log("Result: ".$res['tutorial']);
-        if($res['tutorial'] == 0) {
-            return true;
-        }        
-    } catch (PDOException $exception) {
-        error_log($exception->getMessage());
-        $dbh = null;
-        exit;
-    }
-	return false;
+    if(user_tutorial($user_id) == 0 ) return true;
+    else return false;
 }
 ?>
