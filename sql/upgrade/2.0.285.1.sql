9f07c9a03934f413fd6752143bb6b93bfb23d7af
Ossi Kuisma <52629375+Ninjasoturi@users.noreply.github.com>
2020-11-22 21:35:01 +0200

    Removed unused columns

--- sql/upgrade/2.0.285.1.sql
+++ sql/upgrade/2.0.285.1.sql
@@ -1,4 +1,2 @@
- ALTER TABLE `users` add `trainername_time` DATETIME DEFAULT NULL AFTER `trainername`;
- ALTER TABLE `users` add `trainercode` varchar(12) CHARACTER SET utf8mb4 DEFAULT NULL AFTER `trainername_time`;
- ALTER TABLE `users` add `trainercode_time` DATETIME DEFAULT NULL AFTER `trainercode`;
+ ALTER TABLE `users` add `trainercode` varchar(12) CHARACTER SET utf8mb4 DEFAULT NULL AFTER `trainername`;
  INSERT INTO pokemon (pokedex_id, pokemon_name, pokemon_form_name, pokemon_form_id, asset_suffix, min_cp, max_cp, min_weather_cp, max_weather_cp, weather, shiny) VALUES ('229','Houndoom','mega','-1','51','1432','1505','1790','1882','812','0') 
