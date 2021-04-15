9a4c42f6cfd6e45d41c50fea6862dd417c533f9f
Ninjasoturi <52629375+Ninjasoturi@users.noreply.github.com>
2021-04-15 18:56:56 +0300

    Exclude canceled and done players from alarm

--- logic/sendalarm.php
+++ logic/sendalarm.php
@@ -8,7 +8,7 @@
 function sendalarm($text, $raid, $user)
 {
     // Will fetch all Trainer, which has subscribed for an alarm and send the message
-    $request = my_query("SELECT DISTINCT user_id FROM attendance WHERE raid_id = {$raid} AND alarm = 1");
+    $request = my_query("SELECT DISTINCT user_id FROM attendance WHERE raid_id = {$raid} AND cancel = 0 AND raid_done = 0 AND alarm = 1");
     while($answer = $request->fetch())
     {
         // Only send message for other users!
