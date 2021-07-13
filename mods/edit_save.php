46a3c0656d2f6f85b12d04410015cfaf5b2c7ead
Ninjasoturi <52629375+Ninjasoturi@users.noreply.github.com>
2021-07-13 18:50:52 +0300

    Fixed raid duration changing button

--- mods/edit_save.php
+++ mods/edit_save.php
@@ -96,7 +96,7 @@ if ($update['callback_query']['message']['chat']['type'] == 'private') {
             [
                 [
                     'text'          => getTranslation('change_raid_duration'),
-                    'callback_data' => $id . ':edit_time:0,0,more,1'
+                    'callback_data' => $id . ':edit_time:0,0,0,0,more,1'
                 ]
             ]
         ];
