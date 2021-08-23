7c7bd884b79492cea750071101bde3c698a9bd94
Ninjasoturi <52629375+Ninjasoturi@users.noreply.github.com>
2021-08-23 11:18:03 +0300

    SQL upgrade cleanup In preparation for merger to main - combined all the new database changes to one upgrade file with high enough version number so upgrading from main should work - removed all the pokemon table entry upgrades since they're no longer needed - changed all default character sets to utf8mb4 - cleaned up pokemon-raid-bot.sql from all the mysqldump variables

--- sql/upgrade/2.1.194.6.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE `users` ADD COLUMN IF NOT EXISTS `trainername` varchar(200) CHARACTER SET utf8mb4 DEFAULT NULL AFTER `name`;
-ALTER TABLE `users` ADD COLUMN IF NOT EXISTS `trainercode` varchar(12) CHARACTER SET utf8 DEFAULT NULL AFTER `trainername`;
-ALTER TABLE `users` ADD COLUMN IF NOT EXISTS `tutorial` TINYINT(1) NOT NULL DEFAULT 0 AFTER `level`;
-CREATE TABLE IF NOT EXISTS `user_input` (`id` int(11) NOT NULL AUTO_INCREMENT,`user_id` int(11) DEFAULT NULL,`handler` varchar(45) DEFAULT NULL,`modifiers` text DEFAULT NULL,PRIMARY KEY (`id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-CREATE TABLE IF NOT EXISTS `events` (`id` int(3) unsigned NOT NULL AUTO_INCREMENT,`name` varchar(45) CHARACTER SET latin1 NOT NULL,`description` varchar(200) CHARACTER SET latin1 NOT NULL,`vote_key_mode` int(3) NOT NULL DEFAULT 0,`time_slots` int(3) DEFAULT NULL,`raid_duration` int(3) unsigned NOT NULL DEFAULT 0,`hide_raid_picture` tinyint(1) DEFAULT 0, PRIMARY KEY (`id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-ALTER TABLE `raids` ADD COLUMN IF NOT EXISTS `event` int(3) unsigned DEFAULT NULL AFTER `gender`;
-ALTER TABLE `raids` ADD COLUMN IF NOT EXISTS `event_note` varchar(255) CHARACTER SET utf8 DEFAULT NULL AFTER `event`;
-ALTER TABLE `users` ADD COLUMN IF NOT EXISTS `display_name` INT(1) NOT NULL DEFAULT 0 AFTER `trainername`;
--- sql/upgrade/2.1.224.1.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE `events` ADD COLUMN IF NOT EXISTS `poll_template` VARCHAR(200) NULL DEFAULT NULL AFTER `hide_raid_picture`;
