b7b67473a686283e2bbc818a5b0d2d3b9847ff8e
Ninjasoturi <52629375+Ninjasoturi@users.noreply.github.com>
2021-11-20 17:17:18 +0200

    No need to save the query into a variable

--- logic/disable_raid_level.php
+++ logic/disable_raid_level.php
@@ -7,7 +7,7 @@
 function disable_raid_level($id)
 {
     // Get gym from database
-    $rs = my_query(
+    my_query(
             "
             DELETE FROM raid_bosses
             WHERE       raid_level IN ({$id})
