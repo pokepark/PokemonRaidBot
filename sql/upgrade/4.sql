a09fe13f07df41c25142bdeefd7dd721977ea2e2
Artanicus <artanicus@nocturnal.fi>
2021-12-23 20:13:11 +0200

    Rescue bad default value that got added during dev

--- sql/upgrade/4.sql
+++ sql/upgrade/4.sql
@@ -1,5 +1,7 @@
 ALTER TABLE raids ADD COLUMN IF NOT EXISTS `costume` SMALLINT UNSIGNED NULL DEFAULT 0 AFTER `gender`;
 
+-- For a bit during development the column defaulted to NULL, so if it was already added, we switch the default at least to match.
+ALTER TABLE raid_bosses CHANGE COLUMN IF EXISTS `scheduled` `scheduled` TINYINT(1) NULL DEFAULT 0 AFTER `raid_level`;
 ALTER TABLE raid_bosses ADD COLUMN IF NOT EXISTS `scheduled` TINYINT(1) NULL DEFAULT 0 AFTER `raid_level`;
 
 ALTER TABLE gyms ADD COLUMN IF NOT EXISTS `temporary_gym` TINYINT(1) UNSIGNED NULL DEFAULT 0 AFTER `img_url`;
