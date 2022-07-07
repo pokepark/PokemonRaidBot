5f9cf1ef7c5c703dc1351cc2d080b1d4cb378a2a
ninjasoturi <52629375+Ninjasoturi@users.noreply.github.com>
2022-07-07 20:01:15 +0300

    Fixed php errors if failed to retrieve expected data

--- logic/read_upcoming_bosses.php
+++ logic/read_upcoming_bosses.php
@@ -13,48 +13,50 @@ function read_upcoming_bosses($return_sql = false) {
     $pb_timezone = new dateTimeZone('America/Phoenix');
     $count = 0;
     $sql = $list = $prev_start = $prev_rl = '';
-    foreach($pb['breakingNews'] as $news) {
-        if($news['type'] == 'RAID_TYPE_RAID') {
-            $rl = str_replace('RAID_LEVEL_','', $news['tier']);
-            $raid_level_id = array_search($rl, $pokebattler_level_map);
-            if(!in_array($raid_level_id,$pokebattler_import_future_tiers)) break; // Limit scheduling to tier 5 and mega only
-            $starttime = new DateTime("@".(substr($news['startDate'],0,10)), new dateTimeZone('UTC'));
-            $endtime = new DateTime("@".(substr($news['endDate'],0,10)), new dateTimeZone('UTC'));
+    if(isset($pb['breakingNews'])) {
+        foreach($pb['breakingNews'] as $news) {
+            if($news['type'] == 'RAID_TYPE_RAID') {
+                $rl = str_replace('RAID_LEVEL_','', $news['tier']);
+                $raid_level_id = array_search($rl, $pokebattler_level_map);
+                if(!in_array($raid_level_id,$pokebattler_import_future_tiers)) break; // Limit scheduling to tier 5 and mega only
+                $starttime = new DateTime("@".(substr($news['startDate'],0,10)), new dateTimeZone('UTC'));
+                $endtime = new DateTime("@".(substr($news['endDate'],0,10)), new dateTimeZone('UTC'));
 
-            $starttime->setTimezone($pb_timezone);
-            $endtime->setTimezone($pb_timezone);
+                $starttime->setTimezone($pb_timezone);
+                $endtime->setTimezone($pb_timezone);
 
-            // If the boss only appears for an hour, the eggs most likely start to spawn 20 minutes prior to the time.
-            $diff = $starttime->diff($endtime);
-            if($diff->format('%h') == 1) {
-                $starttime->sub(new DateInterval('PT20M'));
-            }
-            $date_start = $starttime->format('Y-m-d H:i:s');
+                // If the boss only appears for an hour, the eggs most likely start to spawn 20 minutes prior to the time.
+                $diff = $starttime->diff($endtime);
+                if($diff->format('%h') == 1) {
+                    $starttime->sub(new DateInterval('PT20M'));
+                }
+                $date_start = $starttime->format('Y-m-d H:i:s');
 
-            $date_end = $endtime->format('Y-m-d H:i:s');
+                $date_end = $endtime->format('Y-m-d H:i:s');
 
-            $dex_id_form = explode('-',resolve_boss_name_to_ids($news['pokemon']),2);
-            if($prev_start != $date_start) {
-                $list.= CR . EMOJI_CLOCK . ' <b>' . $starttime->format('j.n. ') . getTranslation('raid_egg_opens_at') . $starttime->format(' H:i') . ' —  ' .  $endtime->format('j.n. ') . getTranslation('raid_egg_opens_at') . $endtime->format(' H:i') . ':</b>' . CR;
-                $prev_rl = '';
-            }
-            if($prev_rl != $raid_level_id) {
-                $list.= '<b>' . getTranslation($raid_level_id . 'stars') .':</b>' . CR;
-            }
-            $list.= get_local_pokemon_name($dex_id_form[0], $dex_id_form[1]) . CR;
-            $prev_start = $date_start;
-            $prev_rl = $raid_level_id;
+                $dex_id_form = explode('-',resolve_boss_name_to_ids($news['pokemon']),2);
+                if($prev_start != $date_start) {
+                    $list.= CR . EMOJI_CLOCK . ' <b>' . $starttime->format('j.n. ') . getTranslation('raid_egg_opens_at') . $starttime->format(' H:i') . ' —  ' .  $endtime->format('j.n. ') . getTranslation('raid_egg_opens_at') . $endtime->format(' H:i') . ':</b>' . CR;
+                    $prev_rl = '';
+                }
+                if($prev_rl != $raid_level_id) {
+                    $list.= '<b>' . getTranslation($raid_level_id . 'stars') .':</b>' . CR;
+                }
+                $list.= get_local_pokemon_name($dex_id_form[0], $dex_id_form[1]) . CR;
+                $prev_start = $date_start;
+                $prev_rl = $raid_level_id;
 
-            if($count == 0) {
-                $count++;
-                $sql .= 'INSERT INTO raid_bosses (pokedex_id, pokemon_form_id, date_start, date_end, raid_level, scheduled) VALUES ';
-                $sql .= '("'.$dex_id_form[0].'","'.$dex_id_form[1].'","'.$date_start.'","'.$date_end.'","'.$raid_level_id.'", 1)';
-            }else {
-                $sql .= ',("'.$dex_id_form[0].'","'.$dex_id_form[1].'","'.$date_start.'","'.$date_end.'","'.$raid_level_id.'", 1)';
+                if($count == 0) {
+                    $count++;
+                    $sql .= 'INSERT INTO raid_bosses (pokedex_id, pokemon_form_id, date_start, date_end, raid_level, scheduled) VALUES ';
+                    $sql .= '("'.$dex_id_form[0].'","'.$dex_id_form[1].'","'.$date_start.'","'.$date_end.'","'.$raid_level_id.'", 1)';
+                }else {
+                    $sql .= ',("'.$dex_id_form[0].'","'.$dex_id_form[1].'","'.$date_start.'","'.$date_end.'","'.$raid_level_id.'", 1)';
+                }
             }
         }
+        if($count > 0) $sql.=';';
     }
-    if($count > 0) $sql.=';';
     if($return_sql) return $sql;
     else return $list;
 }
