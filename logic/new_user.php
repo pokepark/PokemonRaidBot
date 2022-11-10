b397274ce9ccf0b6717498fc878f3cfb55298043
ninjasoturi <52629375+Ninjasoturi@users.noreply.github.com>
2022-11-10 13:01:39 +0200

    Fixed new user logic

--- logic/new_user.php
+++ logic/new_user.php
@@ -6,7 +6,7 @@
  */
 function new_user($user_id) {
     global $config;
-    if(!$config->TUTORIAL_MODE || user_tutorial($user_id) < $config->TUTORIAL_LEVEL_REQUIREMENT) return true;
+    if($config->TUTORIAL_MODE && user_tutorial($user_id) < $config->TUTORIAL_LEVEL_REQUIREMENT) return true;
     else return false;
 }
 ?>
