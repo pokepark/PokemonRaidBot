f66df66ffa2a15ea96f117ef8dbf5e9247aca9ae
ninjasoturi <52629375+Ninjasoturi@users.noreply.github.com>
2023-12-04 21:26:24 +0200

    New data source for getdb Pokemon types are no longer in their original order, but I don't think that's an issue

--- mods/getdb.php
+++ mods/getdb.php
@@ -6,8 +6,7 @@ require_once(CORE_BOT_PATH . '/db.php');
 require_once(LOGIC_PATH . '/sql_utils.php');
 require_once(LOGIC_PATH . '/debug.php');
 require_once(LOGIC_PATH . '/curl_get_contents.php');
-$proto_url = 'https://raw.githubusercontent.com/123FLO321/POGOProtos-Swift/master/Sources/POGOProtos/POGOProtos.pb.swift';
-$game_master_url = 'https://raw.githubusercontent.com/PokeMiners/game_masters/master/latest/latest.json';
+$game_master_url = 'https://raw.githubusercontent.com/WatWowMap/Masterfile-Generator/master/master-latest-everything.json';
 
 $error = false;
 
@@ -16,27 +15,15 @@ if(!isset($update)){
   $update = false;
 }
 
-// Read the form ids from protos
-if (!$protos = get_protos($proto_url)) {
-  sendResults('Failed to get protos.', $update, true);
-}
-
-[$form_ids, $form_names, $costume] = $protos;
 // Parse the game master data together with form ids into format we can use
-$pokemon_array = parse_master_into_pokemon_table($form_ids, $form_names, $game_master_url);
+[$pokemon_array, $costume_data] = parse_master_data($game_master_url);
 if(!$pokemon_array) {
   sendResults('Failed to open game master file.', $update, true);
 }
 
 // Save our core datasets to json files for further use
-if(!file_put_contents(ROOT_PATH.'/protos/costume.json', json_encode($costume, JSON_PRETTY_PRINT))) {
+if(!file_put_contents(ROOT_PATH.'/protos/costume.json', json_encode($costume_data, JSON_PRETTY_PRINT))) {
   sendResults('Failed to write costume data to protos/costume.json', $update, true);
-}
-if(!file_put_contents(ROOT_PATH.'/protos/form.json', json_encode($form_ids, JSON_PRETTY_PRINT))) {
-  sendResults('Failed to write form data to protos/form.json', $update, true);
-}
-if(!file_put_contents(ROOT_PATH.'/protos/pokemon.json', json_encode($pokemon_array, JSON_PRETTY_PRINT))) {
-  sendResults('Failed to write pokemon data to protos/pokemon.json', $update, true);
 }
 
 // Craft egg data
@@ -117,193 +104,128 @@ function sendResults($msg, $update, $error = false) {
 function calculate_cps($base_stats) {
   //   CP = (Attack * Defense^0.5 * Stamina^0.5 * CP_Multiplier^2) / 10
   $cp_multiplier = array(20 => 0.5974, 25 => 0.667934);
-  $min = floor((($base_stats['baseAttack']+10)*(($base_stats['baseDefense']+10)**0.5)*(($base_stats['baseStamina']+10)**0.5)*$cp_multiplier[20]**2)/10);
-  $max = floor((($base_stats['baseAttack']+15)*(($base_stats['baseDefense']+15)**0.5)*(($base_stats['baseStamina']+15)**0.5)*$cp_multiplier[20]**2)/10);
-  $min_weather = floor((($base_stats['baseAttack']+10)*(($base_stats['baseDefense']+10)**0.5)*(($base_stats['baseStamina']+10)**0.5)*$cp_multiplier[25]**2)/10);
-  $max_weather = floor((($base_stats['baseAttack']+15)*(($base_stats['baseDefense']+15)**0.5)*(($base_stats['baseStamina']+15)**0.5)*$cp_multiplier[25]**2)/10);
+  $min = floor((($base_stats['attack']+10)*(($base_stats['defense']+10)**0.5)*(($base_stats['stamina']+10)**0.5)*$cp_multiplier[20]**2)/10);
+  $max = floor((($base_stats['attack']+15)*(($base_stats['defense']+15)**0.5)*(($base_stats['stamina']+15)**0.5)*$cp_multiplier[20]**2)/10);
+  $min_weather = floor((($base_stats['attack']+10)*(($base_stats['defense']+10)**0.5)*(($base_stats['stamina']+10)**0.5)*$cp_multiplier[25]**2)/10);
+  $max_weather = floor((($base_stats['attack']+15)*(($base_stats['defense']+15)**0.5)*(($base_stats['stamina']+15)**0.5)*$cp_multiplier[25]**2)/10);
   return [$min,$max,$min_weather,$max_weather];
 }
 
-function get_protos($proto_url) {
-  //Parse the form ID's from pogoprotos
-  if(!$proto_file = curl_get_contents($proto_url)) return false;
-  $proto =  preg_split('/\r\n|\r|\n/', $proto_file);
-  $count = count($proto);
-  $form_ids = $costume = $form_names = array();
-  $data_array = false;
-  for($i=0;$i<$count;$i++) {
-    $line = trim($proto[$i]);
-    if($data_array != false) {
-      $data = explode(':', $line, 2);
-      // End of pokemon data, no need to loop further
-      if(trim($data[0]) == ']') {
-        $data_array = false;
-        if(count($form_ids) > 0 && count($costume) > 0) {
-          // We found what we needed so we can stop looping through proto file and exit
-          break;
-        }
-        continue;
-      }
-      $value = explode('"', $data[1]);
-      if(strlen($value[1]) > 0) {
-        ${$data_array}[trim($value[1])] = trim($data[0]);
-        if($data_array == 'form_ids') {
-          $form_names[trim($data[0])] = trim($value[1]);
-        }
-      }
-      continue;
-    }
-    if($line == 'extension PokemonDisplayProto.Costume: SwiftProtobuf._ProtoNameProviding {') {
-      $data_array = 'costume';
-      $i++; // Jump over one line
-    }
-    if($line == 'extension PokemonDisplayProto.Form: SwiftProtobuf._ProtoNameProviding {') {
-      $data_array = 'form_ids';
-      $i++; // Jump over one line
-    }
-  }
-  unset($proto);
-  return [$form_ids, $form_names, $costume];
-}
-
-function parse_master_into_pokemon_table($form_ids, $form_names, $game_master_url) {
+function parse_master_data($game_master_url) {
   // Set ID's for mega evolutions
   // Using negative to prevent mixup with actual form ID's
   // Collected from pogoprotos (hoping they won't change, so hard coding them here)
-  $mega_ids = array('MEGA' => -1, 'MEGA_X' => -2, 'MEGA_Y' => -3, 'PRIMAL' => -4);
-
+  $mega_names = array(-1 => 'mega', -2 => 'mega_x', -3 => 'mega_y', -4 => 'primal');
+  $pokemon_array = [];
+  $typeArray = [
+    1 => 'normal',
+    2 => 'fighting',
+    3 => 'flying',
+    4 => 'poison',
+    5 => 'ground',
+    6 => 'rock',
+    7 => 'bug',
+    8 => 'ghost',
+    9 => 'steel',
+    10 => 'fire',
+    11 => 'water',
+    12 => 'grass',
+    13 => 'electric',
+    14 => 'psychic',
+    15 => 'ice',
+    16 => 'dragon',
+    17 => 'dark',
+    18 => 'fairy',
+  ];
   $weatherboost_table = array(
-    'POKEMON_TYPE_BUG'    => '3',
-    'POKEMON_TYPE_DARK'   => '8',
-    'POKEMON_TYPE_DRAGON'   => '6',
-    'POKEMON_TYPE_ELECTRIC' => '3',
-    'POKEMON_TYPE_FAIRY'  => '5',
-    'POKEMON_TYPE_FIGHTING' => '5',
-    'POKEMON_TYPE_FIRE'   => '12',
-    'POKEMON_TYPE_FLYING'   => '6',
-    'POKEMON_TYPE_GHOST'  => '8',
-    'POKEMON_TYPE_GRASS'  => '12',
-    'POKEMON_TYPE_GROUND'   => '12',
-    'POKEMON_TYPE_ICE'    => '7',
-    'POKEMON_TYPE_NORMAL'   => '4',
-    'POKEMON_TYPE_POISON'   => '5',
-    'POKEMON_TYPE_PSYCHIC'  => '6',
-    'POKEMON_TYPE_ROCK'   => '4',
-    'POKEMON_TYPE_STEEL'  => '7',
-    'POKEMON_TYPE_WATER'  => '3'
+    1 => '4',
+    2 => '5',
+    3 => '6',
+    4 => '5',
+    5 => '12',
+    6 => '4',
+    7 => '3',
+    8 => '8',
+    9 => '7',
+    10 => '12',
+    11 => '3',
+    12 => '12',
+    13 => '3',
+    14 => '6',
+    15 => '7',
+    16 => '6',
+    17 => '8',
+    18 => '5',
   );
   if(!$master_file = curl_get_contents($game_master_url)) return false;
   $master = json_decode($master_file, true);
-  foreach($master as $row) {
-    $part = explode('_', $row['templateId']);
-    $form_data = [];
-    $pokemon_id = '';
-    if(count($part) < 2) continue;
-    if(preg_match('/FORMS_V([0-9]*)_POKEMON_([a-zA-Z0-9_]*)/', $row['templateId'], $matches)) {
-      // Found Pokemon form data
-      $pokemon_id = (int)$matches[1];
-      $pokemon_name = $matches[2];
-      // Get pokemon forms
-      if(!isset($row['data']['formSettings']['forms']) or empty($row['data']['formSettings']['forms'][0])) {
-        $form_data[] = array('form' => $pokemon_name . '_NORMAL');
-      }else {
-        $form_data = $row['data']['formSettings']['forms'];
-      }
-      foreach($form_data as $form) {
-        // This is some new niantic shit
-        $pokemon_form_name = $form['form'];
-        if(is_int($form['form'])) {
-          $pokemon_form_name = $form_names[$form['form']];
-        }
-        $form_name = strtolower(str_replace($pokemon_name.'_','',$pokemon_form_name));
-        if($form_name == 'purified' || $form_name == 'shadow') continue;
-        $poke_name = ucfirst(strtolower($row['data']['formSettings']['pokemon']));
-        $form_id = $form_ids[$pokemon_form_name] ?? 0;
-
-        $pokemon_array[$pokemon_id][$form_name] = [
-          'pokemon_name' => $poke_name,
-          'pokemon_form_name' => $form_name,
-          'pokemon_form_id' => $form_id,
-        ];
-      }
-    }else if (preg_match('/V([0-9]*)_POKEMON_([a-zA-Z0-9_]*)/', $row['templateId'], $matches) && isset($row['data']['pokemonSettings'])) {
-      // Found Pokemon data
-      $pokemon_id = (int)$matches[1];
-      $form_name = str_replace($row['data']['pokemonSettings']['pokemonId'] . '_', '', $matches[2]);
-      if($form_name == 'PURIFIED' || $form_name == 'SHADOW' || $form_name == 'NORMAL'
-        || !isset($pokemon_array[$pokemon_id])
-        || !isset($row['data']['pokemonSettings']['stats']['baseAttack'])
-        || !isset($row['data']['pokemonSettings']['stats']['baseDefense'])
-        || !isset($row['data']['pokemonSettings']['stats']['baseStamina'])
-      ) {
+  foreach($master['pokemon'] as $row) {
+    $pokemon_id = $row['pokedexId'];
+    $pokemon_name = $row['name'];
+    if(!isset($row['stats']['attack']) || !isset($row['stats']['defense']) || !isset($row['stats']['stamina'])) {
+      continue;
+    }
+    [$min_cp, $max_cp, $min_weather_cp, $max_weather_cp] = calculate_cps($row['stats']);
+    $type = $type2 = '';
+    foreach($row['types'] as $key => $data) {
+      if($type == '') {
+        $type = $typeArray[$key];
+        $weather = $weatherboost_table[$key];
         continue;
       }
-      if($form_name != $row['data']['pokemonSettings']['pokemonId']) {
-        $form_name = strtolower($form_name);
-      }else {
-        $form_name = 'normal';
-      }
-      [$min_cp, $max_cp, $min_weather_cp, $max_weather_cp] = calculate_cps($row['data']['pokemonSettings']['stats']);
-
-      $type = strtolower(str_replace('POKEMON_TYPE_', '', $row['data']['pokemonSettings']['type']));
-      $type2 = '';
-
-      $weather = $weatherboost_table[$row['data']['pokemonSettings']['type']];
-      if(isset($row['data']['pokemonSettings']['type2'])) {
-        $type2 = strtolower(str_replace('POKEMON_TYPE_', '', $row['data']['pokemonSettings']['type2']));
-
-        # Add type2 weather boost only if there is a second type and it's not the same weather as the first type!
-        if($weatherboost_table[$row['data']['pokemonSettings']['type2']] != $weatherboost_table[$row['data']['pokemonSettings']['type']]) {
-          $weather .= $weatherboost_table[$row['data']['pokemonSettings']['type2']];
+      $type2 = $typeArray[$key];
+      $weather .= $weatherboost_table[$key];
+    }
+    foreach($row['forms'] as $formId => $formData) {
+      if($formId == 0 || $formData['name'] == 'Shadow' || $formData['name'] == 'Purified') continue;
+      $form_name = strtolower(preg_replace('/\s/', '_', $formData['name']));
+      $form_id = $formData['form'];
+      $pokemon_array[$pokemon_id][$form_name] = [
+        'pokemon_name'      => $pokemon_name,
+        'pokemon_form_name' => $form_name,
+        'pokemon_form_id'   => $form_id,
+        'min_cp'            => $min_cp,
+        'max_cp'            => $max_cp,
+        'min_weather_cp'    => $min_weather_cp,
+        'max_weather_cp'    => $max_weather_cp,
+        'weather'           => $weather,
+        'type'              => $type,
+        'type2'             => $type2,
+      ];
+    }
+    if(!isset($row['tempEvolutions'])) continue;
+    foreach($row['tempEvolutions'] as $tempData) {
+      if(isset($tempData['unreleased']) && $tempData['unreleased']) continue;
+      $form_id = -$tempData['tempEvoId'];
+      $form_name = $mega_names[$form_id];
+      if(isset($tempData['types'])) {
+        foreach($tempData['types'] as $key => $data) {
+          if($type == '') {
+            $type = $key;
+            $weather = $weatherboost_table[$key];
+            continue;
+          }
+          $type2 = $key;
+          $weather .= $weatherboost_table[$key];
         }
       }
-      if(isset($pokemon_array[$pokemon_id][$form_name])) {
-        $pokemon_array[$pokemon_id][$form_name]['min_cp'] = $min_cp;
-        $pokemon_array[$pokemon_id][$form_name]['max_cp'] = $max_cp;
-        $pokemon_array[$pokemon_id][$form_name]['min_weather_cp'] = $min_weather_cp;
-        $pokemon_array[$pokemon_id][$form_name]['max_weather_cp'] = $max_weather_cp;
-        $pokemon_array[$pokemon_id][$form_name]['weather'] = $weather;
-        $pokemon_array[$pokemon_id][$form_name]['type'] = $type;
-        $pokemon_array[$pokemon_id][$form_name]['type2'] = $type2;
-      }else {
-        // Fill data for Pokemon that have form data but no stats for forms specifically
-        foreach($pokemon_array[$pokemon_id] as $form => $data) {
-          $pokemon_array[$pokemon_id][$form]['min_cp'] = $min_cp;
-          $pokemon_array[$pokemon_id][$form]['max_cp'] = $max_cp;
-          $pokemon_array[$pokemon_id][$form]['min_weather_cp'] = $min_weather_cp;
-          $pokemon_array[$pokemon_id][$form]['max_weather_cp'] = $max_weather_cp;
-          $pokemon_array[$pokemon_id][$form]['weather'] = $weather;
-          $pokemon_array[$pokemon_id][$form]['type'] = $type;
-          $pokemon_array[$pokemon_id][$form]['type2'] = $type2;
-        }
-      }
-      if(!isset($row['data']['pokemonSettings']['tempEvoOverrides'])) continue;
-      foreach($row['data']['pokemonSettings']['tempEvoOverrides'] as $temp_evolution) {
-        if(!isset($temp_evolution['tempEvoId'])) continue;
-
-        $mega_evolution_name = str_replace('TEMP_EVOLUTION_', '', $temp_evolution['tempEvoId']);
-        // We only override the types for megas
-        // weather info is used to display boosts for caught mons, which often are different from mega's typing
-        $typeOverride = strtolower(str_replace('POKEMON_TYPE_', '', $temp_evolution['typeOverride1']));
-        $typeOverride2 = '';
-
-        if(isset($temp_evolution['typeOverride2'])) {
-          $typeOverride2 = strtolower(str_replace('POKEMON_TYPE_', '', $temp_evolution['typeOverride2']));
-        }
-        $pokemon_array[$pokemon_id][$mega_evolution_name] = [
-          'pokemon_name'    => $pokemon_array[$pokemon_id][$form_name]['pokemon_name'],
-          'pokemon_form_name' => strtolower($mega_evolution_name),
-          'pokemon_form_id'   => $mega_ids[$mega_evolution_name],
-          'min_cp'      => $min_cp,
-          'max_cp'      => $max_cp,
-          'min_weather_cp'  => $min_weather_cp,
-          'max_weather_cp'  => $max_weather_cp,
-          'weather'       => $weather,
-          'type'        => $typeOverride,
-          'type2'       => $typeOverride2,
-        ];
-      }
+      $pokemon_array[$pokemon_id][$form_name] = [
+        'pokemon_name'      => $pokemon_name,
+        'pokemon_form_name' => $form_name,
+        'pokemon_form_id'   => $form_id,
+        'min_cp'            => $min_cp,
+        'max_cp'            => $max_cp,
+        'min_weather_cp'    => $min_weather_cp,
+        'max_weather_cp'    => $max_weather_cp,
+        'weather'           => $weather,
+        'type'              => $type,
+        'type2'             => $type2,
+      ];
     }
   }
-  return $pokemon_array;
+  $costume_data = [];
+  foreach($master['costumes'] as $costume) {
+    $costume_data[$costume['proto']] = $costume['id'];
+  }
+  return [$pokemon_array, $costume_data];
 }
