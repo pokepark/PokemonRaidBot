abeb5c2bb2e6dff8e16655d0b0fc820af027f98d
ninjasoturi <52629375+Ninjasoturi@users.noreply.github.com>
2024-02-13 20:26:57 +0200

    Renamed variable for consistency

--- mods/trainer_share.php
+++ mods/trainer_share.php
@@ -13,7 +13,7 @@ require_once(LOGIC_PATH . '/config_chats.php');
 $botUser->accessCheck('trainer-share');
 
 // Get chat object by chat short id
-$chat = get_config_chat_by_short_id($data['c']);
+$chatObj = get_config_chat_by_short_id($data['c']);
 
 // Get text and keys.
 $text = show_trainerinfo($update);
@@ -23,7 +23,7 @@ $keys = keys_trainerinfo();
 $tg_json = array();
 
 // Send the message.
-$tg_json[] = send_message($chat, $text, $keys, ['disable_web_page_preview' => 'true'], true, 'trainer');
+$tg_json[] = send_message($chatObj, $text, $keys, ['disable_web_page_preview' => 'true'], true, 'trainer');
 
 // Set callback keys and message
 $callback_msg = getTranslation('successfully_shared');
