5afd453666331f79dd1b93d401304d2ebc49ba87
ninjasoturi <52629375+Ninjasoturi@users.noreply.github.com>
2024-02-20 20:46:45 +0200

    More cleanup logic fixing

--- core/bot/cleanup_collect.php
+++ core/bot/cleanup_collect.php
@@ -20,8 +20,15 @@ if($message != null) {
   $message_id = $message['message_id'];
   $thread_id = $message['message_thread_id'] ?? NULL;
   if(isset($message['reply_markup']['inline_keyboard'])) {
-    $split_data = explode(':', $message['reply_markup']['inline_keyboard'][0][0]['callback_data']);
-    $cleanup_id = $split_data[0];
+    $splitData = explode('|', $message['reply_markup']['inline_keyboard'][0][0]['callback_data']);
+    // Search for raid id in the first button of the message
+    for($i=1;$i<count($splitData);$i++) {
+      $splitVariable = explode('=', $splitData[$i], 2);
+      if(count($splitVariable) == 2 && $splitVariable[0] == 'r' && preg_match("^[0-9]+^", $splitVariable[1])) {
+        $cleanup_id = $splitVariable[1];
+        break;
+      }
+    }
   }else {
     // Get id from text.
     $idFromText = substr($message['text'],strpos($message['text'], substr(strtoupper($config->BOT_ID), 0, 1) . '-ID = ') + 7);
@@ -31,9 +38,9 @@ if($message != null) {
   }
 
   // Write cleanup info to database.
-  cleanup_log('Calling cleanup preparation now!');
-  cleanup_log('Cleanup_ID: ' . $cleanup_id);
   if($cleanup_id != 0) {
+    cleanup_log('Calling cleanup preparation now!');
+    cleanup_log('Cleanup_ID: ' . $cleanup_id);
     require_once(LOGIC_PATH . '/insert_cleanup.php');
     insert_cleanup($chat_id, $message_id, $thread_id, $cleanup_id, 'inline_poll_text');
   }
